e9f33cea15b946f1ade584dd9b76f64efc922eb7
JoniMora <yoniiran@gmail.com>
2024-06-11 12:00:51 -0300

    Walker - attempt 1

--- BaseCompiladores/src/main/java/compiladores/Caminante.java
+++ BaseCompiladores/src/main/java/compiladores/Caminante.java
@@ -1,41 +1,57 @@
 package compiladores;
 
-import compiladores.compiladoresParser.ExpContext;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import compiladores.compiladoresParser.AsignacionContext;
+import compiladores.compiladoresParser.ProgramaContext;
 import compiladores.compiladoresParser.ExpresionContext;
 import compiladores.compiladoresParser.FactorContext;
-import compiladores.compiladoresParser.ProgramaContext;
+import compiladores.compiladoresParser.ExpContext;
 
 public class Caminante extends compiladoresBaseVisitor<String> {
 
+    private Integer asignaciones = 0;
+
     @Override
-    public String visitPrograma(ProgramaContext arg0) {
-        // TODO Auto-generated method stub
-        System.out.println("Comenzamos a caminar el arbol");
-        return super.visitPrograma(arg0);
+    public String visitPrograma(ProgramaContext ctx) {
+        System.out.println("Comenzamos a caminar el árbol");
+        String ret = super.visitPrograma(ctx);
+        System.out.println("Se realizaron " + asignaciones + " asignaciones");
+        return ret;
     }
 
     @Override
-    public String visitExpresion(ExpresionContext arg0) {
-        // TODO Auto-generated method stub
+    public String visitAsignacion(AsignacionContext ctx) {
+        asignaciones++;
+        System.out.println("Soy un nodo y tengo : " + ctx.getChildCount() + " hijos.");
+        System.out.println(" -> asignando valor a " + ctx.getChild(0).getText());
+        return ctx.getText();
+    }
+
+    @Override
+    public String visitExpresion(ExpresionContext ctx) {
         System.out.println("Llegamos a las expresiones");
-        return super.visitExpresion(arg0);
+        return super.visitExpresion(ctx);
     }
 
     @Override
-    public String visitFactor(FactorContext arg0) {
-        // TODO Auto-generated method stub
-        System.out.println("FACTOR tiene " + arg0.getChildCount() + " hijos - texto " + arg0.getText());
-        if (arg0.getChildCount() == 3) {
+    public String visitFactor(FactorContext ctx) {
+        System.out.println("FACTOR tiene " + ctx.getChildCount() + " hijos - texto " + ctx.getText());
+        if (ctx.getChildCount() == 3) {
             System.out.println("Salto a exp");
-            return super.visitExp((ExpContext)arg0.getChild(1));
+            return super.visitExp((ExpContext) ctx.getChild(1));
         }
-        return super.visitFactor(arg0);
+        return super.visitFactor(ctx);
     }
 
     @Override
-    public String visitExp(ExpContext arg0) {
-        // TODO Auto-generated method stub
+    public String visitExp(ExpContext ctx) {
         System.out.println("Llegamos a las exp");
-        return super.visitExp(arg0);
-    }    
+        return super.visitExp(ctx);
+    }
+
+    @Override
+    public String visitTerminal(TerminalNode node) {
+        // System.out.println("Soy una hoja y tengo el token: " + node.getText());
+        return super.visitTerminal(node);
+    }
 }
